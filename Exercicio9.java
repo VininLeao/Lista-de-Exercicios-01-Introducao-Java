5cd538df53f02c9288d1563047ab239db824446e
VininLeao <104996741+VininLeao@users.noreply.github.com>
2023-08-05 20:29:05 -0300

    Exercício 15

---
+++ Exercicio9.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Exercicio9 {
+    public static void main(String[] args) {
+        double cont = 0;
+        double soma = 0;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite 50 números:");
+
+        do {
+            double n = scanner.nextDouble();
+            soma += n;
+            cont++;
+        } while (cont < 50);
+
+        double media = (soma / cont);
+
+        System.out.println("A soma entre os 50 números é: " +soma);
+        System.out.println("A média aritmética entre os 50 números é: " +media);
+
+    }
+}
+
